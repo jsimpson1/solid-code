ee2b5045db62747dfd77c41ce38b01c5684fd256
Jeremy Simpson <jeremy@accur8software.com>
2016-03-23 08:20:05 -0400

    2/23/16, up to chapter 2.5

--- functional-programming/chapter2/MyModule.scala
+++ functional-programming/chapter2/MyModule.scala
@@ -1,5 +1,25 @@
 object MyModule {
 
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42)) 
+    println(formatFactorial(7))
+  }
+  
+  def formatResult(name: String, n: Int, f: Int => Int) = { 
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d." 
+    msg.format(x, abs(x))
+  }
+  
+  private def formatFactorial(n: Int) = { 
+    val msg = "The factorial of %d is %d." 
+    msg.format(n, factorial(n))
+  }
+
   def abs(n: Int): Int = {
     if ( n < 0 ) -n
     else n
@@ -15,11 +35,13 @@ object MyModule {
   }
 
   def fib(n: Int): Int = {
-    def go(nn: Int, prev: Int): Int = {
-      if (nn < n) go(nn+1, prev + (nn+1) )
-      else prev
+    def go(i: Int, j: Int, k: Int): Int = {
+      if (n == 0) 0
+      else if (n == 1 || n == 2) 1
+      else if ( i < n ) go(i+1, k, j+k)
+      else j + k
     }
-    go(0, 0)
+    go(1, 0, 1)
   }
 
 }
